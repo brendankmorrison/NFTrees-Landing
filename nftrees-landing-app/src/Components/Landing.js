588b73c1be0cac35ecb077203cef6749c06afba3
Brendan Morrison <b.k.morrison2@gmail.com>
2021-05-28 19:38:54 -0700

    add links

--- nftrees-landing-app/src/Components/Landing.js
+++ nftrees-landing-app/src/Components/Landing.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Landing.css';
 import background from '../background.jpg';
+import { FaDiscord } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 function Landing (props){
 
     return(
         <div className = 'landingContainer' id = 'landing'>
             <div className = 'landingContent'>
+                <div className = 'links'>
+                    <a className = 'link'><FaTwitter size={30} fill = {'rgb(199, 199, 199)'} className = 'icon'/></a>
+                    <a className = 'link'><FaDiscord size={30} fill = {'rgb(199, 199, 199)'} className = 'icon'/></a>
+                </div>
+                <div className = 'spacer'/>
                 <div className = 'title'>NFTrees</div>
+                <div className = 'underlineContainer'>
+                    <div className = 'underline'></div>
+                </div>
                 <p className = 'information'>
                     The first NFT-based carbon credit platform.
                     <br></br>
@@ -16,9 +26,12 @@ function Landing (props){
                     Displace your protocol's carbon emmissions with NFTrees.  
 
                 </p>
+
                 <p className = 'comingSoon'>
-                    coming soon
+                        coming soon
                 </p>
+                <p className = 'contact'>contact us at team@NFTrees.com</p>
+
             </div>
         </div>
     );
